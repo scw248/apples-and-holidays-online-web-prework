bb01800ed2a8c7cb5be0e6b9c7720848bebfe16e
Stephen Wright <scw248@gmail.com>
2019-01-06 19:12:36 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -69,7 +69,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday| 
     puts "#{season.capitalize}:"
     holiday.each do |holiday, items|
-        holiday_names = holiday.split('_')
+        holiday_names = holiday.to_s.split('_')
         holiday_names.each { |name| name.capitalize }
         puts "  #{holiday_names.join(" ")}: #{items.join(', ')}"
       #else
